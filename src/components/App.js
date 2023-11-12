e3c7c459e17576cc0a4a6f2b8af6705804e61779
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2023-11-12 11:03:24 -0500

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 // 👉 TASK 1 - import the axios lib from node_modules
+import axios from 'axios'
 
 // 👉 TASK 2 - import the contants from constants/index.js
+
+import { BASE_URL, API_KEY } from '../constants/index';
 
 import Details from './Details'
 import Friend from './Friend';
